f397a46c1726556c83647330ec20a4d2659c4791
Paul Ashour <paul.ashour@gmail.com>
2018-09-09 16:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions and code here
+let kittens = new Array();
+destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+destructivelyPrependKitten(name) {
+  kittens.shift(name);
+}
